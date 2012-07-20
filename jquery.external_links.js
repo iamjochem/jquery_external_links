00de075773a478cd244fb0ddabe81addb112439b
Jochem Maas <jochem@iamjochem.com>
2012-07-20 14:34:10 +0200

    improvements: bugfix use of $.isObject, clean up code a bit, allow multiple special_hosts to reference same hostname, decrease likelyhood that the destroy method destroys something not related to to the plugin

--- jquery.external_links.js
+++ jquery.external_links.js
@@ -2,16 +2,19 @@
     var datakey = '$external-link-data';
 
     var defopts = {
+        favicon         : true,
         favicon_url     : 'http://favicon.yandex.net/favicon',
         url_seperator   : '/',
         link_class      : 'external-link',
         // the following means only allow 'A' dom elements will be effected by this plugin (by default)
         link_selector   : 'a',
         span_class      : 'ext-favicon',
-        span_position   : 'right', // left or right (determines where the span is inject relative to the link)
-        special_hosts   : {
-            'github.com': /github\.com$/
-        }
+        // left or right (determines where the span is inject relative to the link)
+        span_position   : 'right',
+        // array of regexps and alternative hostnames to use if a URLs host matches the corresponding regexp
+        special_hosts   : [
+            [/github\.com$/, 'github.com']
+        ]
     };
 
     var methods = {
@@ -19,10 +22,13 @@
             if (this.data(datakey))
                 return this; // already initialized
 
-            opts =  $.extend(true, {}, defopts, opts);
+            if (opts && $.isArray(opts.special_hosts))
+                $.merge(opts.special_hosts, defopts.special_hosts);
+
+            opts = $.extend({}, defopts, opts);
 
             var manipFn = 'prependTo', // manipulation method to use when injecting the favicon span elements
-                url     = opts.favicon_url,
+                url     = opts.favicon ? opts.favicon_url : null,
                 finks   = [],
                 furls   = [],
                 items   = (typeof opts.link_selector === 'string' && opts.link_selector.length)
@@ -44,32 +50,42 @@
                 var $this = $(this),
                     host  = this.hostname;
 
-                for (var shost in opts.special_hosts)
-                    if (opts.special_hosts.hasOwnProperty(shost) && host.match(opts.special_hosts[shost]))
-                        host = shost;
+                if (url) {
+                    if ($.isArray(opts.special_hosts))
+                        $.each(opts.special_hosts, function(i, v) {
+                            if (!$.isArray(v) || v.length !== 2)
+                                return;
 
-                var idx  = furls.indexOf(host);
+                            if (host.match(v[0]))
+                                host = v[1];
+                        });
 
-                if (idx === -1) {
-                    url     += opts.url_seperator + host;
-                    finks[n] = furls.length;
-                    furls.push(host);
-                } else {
-                    finks[n] = idx;
+                    var idx  = furls.indexOf(host);
+
+                    if (idx === -1) {
+                        url     += opts.url_seperator + host;
+                        finks[n] = furls.length;
+                        furls.push(host);
+                    } else {
+                        finks[n] = idx;
+                    }
                 }
 
                 $this.data(datakey, {
                     link_class      : opts.link_class,
-                    span_class      : opts.span_class,
-                    orig_target     : $this.attr('target')
+                    span_class      : url ? opts.span_class : null,
+                    orig_target     : $this.attr('target'),
+                    favicon_url     : url,
+                    span_position   : opts.span_position === 'right' ? 'right' : 'left'
                 });
-            })
-            // add the favicon element
-            .each(function(n) {
-                $('<span class="' + opts.span_class + '" style="background-image: url(' + url + ');background-position : right -' + finks[n] * 16 + 'px;"></span>')[manipFn](this);
             })
             // set target attribute to point to new window
             .attr('target', '_blank');
+
+            // add the favicon element
+            if (url) items.each(function(n) {
+                $('<span class="' + opts.span_class + '" style="background-image: url(' + url + ');background-position : right -' + finks[n] * 16 + 'px;"></span>')[manipFn](this);
+            })
 
             // allow jQ method-chaining :)
             return this;
@@ -80,11 +96,18 @@
                 var $this = $(this),
                     opts  = $this.data(datakey);
 
-                if (!$.isObject(opts))
+                if (!$.isPlainObject(opts) || !opts.span_position)
                     return;
 
-                if (opts.link_class) $this.removeClass(opts.link_class);
-                if (opts.span_class) $this.children('.' + opts.span_class).remove();
+                if (opts.link_class)
+                    $this.removeClass(opts.link_class);
+
+                if (opts.favicon_url) {
+                    var css_class  = opts.span_class ? '.' + opts.span_class : '',
+                        css_psuedo = opts.span_position === 'right' ? ':last-child' : ':first-child';
+
+                    $this.children('span' + css_psuedo + css_class).remove();
+                }
 
                 $this.attr('target', opts.orig_target ? opts.orig_target : null);
             });
